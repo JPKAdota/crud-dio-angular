7cdb58141fb0cb81ab9ac6128661b1b125a4f24d
Kadota <joaokadota@gmail.com>
2022-08-02 22:18:12 -0300

    Please enter the commit message for your changes. Lines starting  with '' will be ignored, and an empty message aborts the commit.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   src/app/app-routing.module.ts
    	modified:   src/app/app.module.ts
    	new file:   src/app/pages/pipes-example/pipes-example.component.css
    	new file:   src/app/pages/pipes-example/pipes-example.component.html
    	new file:   src/app/pages/pipes-example/pipes-example.component.spec.ts
    	new file:   src/app/pages/pipes-example/pipes-example.component.ts
    	new file:   src/app/print-list.pipe.spec.ts
    	new file:   src/app/print-list.pipe.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,6 +20,9 @@ import { TodoItemComponent } from './pages/todo/todo-item/todo-item.component';
 import { TodoListComponent } from './pages/todo/todo-list/todo-list.component';
 import { ButtonComponent } from './components/button/button.component';
 import { LifeCycleComponent } from './components/life-cycle/life-cycle.component';
+import { PipesExampleComponent } from './pages/pipes-example/pipes-example.component';
+import { UpperCasePipe } from '@angular/common';
+import { PrintListPipe } from './print-list.pipe';
 
 @NgModule({
   declarations: [
@@ -31,7 +34,9 @@ import { LifeCycleComponent } from './components/life-cycle/life-cycle.component
     TodoItemComponent,
     TodoListComponent,
     ButtonComponent,
-    LifeCycleComponent
+    LifeCycleComponent,
+    PipesExampleComponent,
+    PrintListPipe
   ],
   imports: [
     BrowserModule,
@@ -48,7 +53,7 @@ import { LifeCycleComponent } from './components/life-cycle/life-cycle.component
     MatIconModule
 
   ],
-  providers: [],
+  providers: [UpperCasePipe],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
